9568752c8d0316c352c7a111bac4cbe694c07422
jimmyzhou <2001210724@stu.pku.edu.cn>
2021-01-01 20:57:01 +0800

    newtheme

--- source/conf.py
+++ source/conf.py
@@ -55,11 +55,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes']
+html_theme = 'dou'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 master_doc = 'index'
