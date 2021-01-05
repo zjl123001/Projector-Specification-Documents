d5bce3d03e760bde1280c175ae433693e6129547
jimmyzhou <2001210724@stu.pku.edu.cn>
2021-01-06 00:31:04 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,8 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages','rst2pdf.pdfbuilder']
+extensions = ['sphinx.ext.githubpages']
+#extensions = ['sphinx.ext.githubpages','rst2pdf.pdfbuilder']
 
 
 
